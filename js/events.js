3f2db6fd7da75263f9c44232658c50cf23d2b97b
Lucien Boudy <luboudy@gmail.com>
2015-10-13 19:32:48 +0200

    fix pause

--- js/events.js
+++ js/events.js
@@ -5,23 +5,12 @@ function init_events (config) {
 	render_canvas.requestPointerLock = render_canvas.requestPointerLock || render_canvas.msRequestPointerLock || render_canvas.mozRequestPointerLock || render_canvas.webkitRequestPointerLock;
 
 	document.addEventListener("click", function (evt) {
-		if (config.is_game_paused && render_canvas.requestPointerLock) {
+		if (render_canvas.requestPointerLock) {
+			config.is_game_paused = false;
 			render_canvas.requestPointerLock();
 		}
 		config.player.bindedFire(evt);
 	}, false);
-
-	function pointerLockChange () {
-
-		if (document.pointerLockElement) {
-			config.is_game_paused = false;
-		} else {
-			config.is_game_paused = true;
-		}
-	}
-	document.addEventListener("pointerlockchange", pointerLockChange, false);
-	document.addEventListener("mozpointerlockchange", pointerLockChange, false);
-	document.addEventListener("webkitpointerlockchange", pointerLockChange, false);
 
 	document.addEventListener("keydown", function (evt) {
 		config.player.onKeyDown(evt.keyCode);
