04181f80c73a15c6a74b9b8274d2eef79369ec3d
Maxime Lo Re <solfen@hotmail.com>
2015-10-26 19:19:09 +0100

    more drugs = more score

--- js/AIManager.js
+++ js/AIManager.js
@@ -38,7 +38,7 @@ AIManager.prototype.killAI = function(name, isFromPlayer) {
 
 	if(isFromPlayer) {
 		this.config.DrugPillsManager.spawnDrug(this.AIs[name].mesh.position.x, this.AIs[name].mesh.position.z);
-		this.config.score += this.config.pointPerEnemyKilled;
+		this.config.score += this.config.pointPerEnemyKilled * (config.drug.drug_lvl+1) | 0;
 		this.config.ParticlesManager.launch("AIDeath", this.AIs[name].mesh.position);
 	}
 
